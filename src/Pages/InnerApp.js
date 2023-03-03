c581a44600bbc6499276ecceaf91939acd216461
peng.yan <peng.yan@ticketingbox.com>
2023-03-03 10:29:55 -0500

    test

--- src/Pages/InnerApp.js
+++ src/Pages/InnerApp.js
@@ -4,7 +4,7 @@ function InnerApp() {
   return (
     <div>
       <h1>Hello</h1>
-      <h3>Welcome to App</h3>
+      <h3>Welcome to App!</h3>
       <button onClick={() => Auth.signOut()}>Sign Out</button>
     </div>
   )
